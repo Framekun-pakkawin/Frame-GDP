be5975ccf84c5a988873bcbb2ef56d10f96799ff
kuguardt <kuguardt@gmail.com>
2021-02-03 21:02:55 +0700

    RenderingTest

---
+++ Assets/ScriptPlayer/CharacterSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSwitch : MonoBehaviour
+{
+    public bool ControlingP1 = true;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (ControlingP1 == true)
+            {
+                ControlingP1 = false;
+            }
+            else
+            {
+                ControlingP1 = true;
+            }
+        }
+        
+    }
+}
---
+++ Assets/ScriptPlayer/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    float horizontalmove = 0.0f;
+    public float runspeed = 3.0f;
+    bool jump = false;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        horizontalmove = Input.GetAxisRaw("Horizontal")* runspeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalmove * Time.deltaTime,false,jump);
+        jump = false;
+    }
+}
