1d0376f35f695b792d6197e0d44859bf3d4b5a8b
kuguardt <kuguardt@gmail.com>
2021-02-16 15:27:15 +0700

    lightsaber shooting

---
+++ Assets/ScriptPlayer/Player2Attack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Attack : MonoBehaviour
+{
+    public Transform firepoint;
+    public GameObject bulletPrefab;
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Attack2"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        Instantiate(bulletPrefab,firepoint.position,firepoint.rotation);
+    }
+}
---
+++ Assets/ScriptPlayer/bulletsaber.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletsaber : MonoBehaviour
+{
+    public float speed = 20.0f;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        
+        Destroy(gameObject);
+    }
+}
