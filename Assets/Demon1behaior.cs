a3133d8a1987d501338bcd586a1b02d80db9aa80
kuguardt <kuguardt@gmail.com>
2021-03-12 18:18:33 +0700

    test bug anim2d

---
+++ Assets/Demon1behaior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demon1behaior : MonoBehaviour
+{
+    //Demon1behaiorDemon1behaiorDemon1behaiorDemon1behaiorDemon1behaiorDemon1behaiorDemon1behaiorDemon1behaiorDemon1behaiorDemon1behaiorDemon1behaiorDemon1behaiorDemon1behaior
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
