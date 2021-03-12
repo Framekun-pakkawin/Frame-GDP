19b9eb6a52fab7ae8da0b0becfbc31024de7454c
kingfolk7 <inwking7@gmail.com>
2021-03-12 17:59:22 +0700

    dead and pause

---
+++ Assets/DeathMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public string mainMenuLevel1;
+
+    public void RestartGame()
+    {
+
+    }
+
+    public void QuitToMain()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
