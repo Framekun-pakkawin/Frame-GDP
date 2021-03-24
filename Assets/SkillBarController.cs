1caeb40d75a1f139c7a7254df24bdc1265bba758
kingfolk7 <inwking7@gmail.com>
2021-03-24 16:40:28 +0700

    SkillCooldown

---
+++ Assets/SkillBarController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SkillBarController : MonoBehaviour
+{
+    public Image imageCooldown;
+    public float cooldown = 5;
+    bool isCooldown;
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Alpha1))
+        {
+            isCooldown = true;
+        }
+
+        if(isCooldown)
+        {
+            imageCooldown.fillAmount += 1 / cooldown * Time.deltaTime;
+
+            if(imageCooldown.fillAmount == 1)
+            {
+                imageCooldown.fillAmount = 0;
+                isCooldown = false;
+            }
+        }
+
+    }
+}
