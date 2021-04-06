f1a1122a63b335cfac29d7f15375197e84da7def
kingfolk7 <inwking7@gmail.com>
2021-04-06 20:44:12 +0700

    NewTutorial

--- Assets/ScriptPlayer/TutorialManager.cs
+++ Assets/ScriptPlayer/TutorialManager.cs
@@ -6,60 +6,72 @@ public class TutorialManager : MonoBehaviour
 {
     public GameObject[] popUps;
     public int popUpIndex;
-
-    private void Update()
+    public float Delay = 2.00f;
+    bool isDelay = false;
+    IEnumerator pressDelay()
     {
-        for (int i = 0; i < popUps.Length; i++)
-        {
-            if (i == popUpIndex)
-            {
-                popUps[popUpIndex].SetActive(true);
-            }
-            else
-            {
-                popUps[popUpIndex].SetActive(false);
-            }
-        }
+        isDelay = true;
 
+        yield return new WaitForSeconds(Delay);
+        isDelay = false;
+       
+    }
+    void changeIndex()
+    {
+        if (isDelay == false)
+        {
+            popUps[popUpIndex].SetActive(false);
+            popUpIndex++;
+            popUps[popUpIndex].SetActive(true);
+            StartCoroutine(pressDelay());
+        }
+    }
+
+    void Update()
+    {
         if (popUpIndex == 0)
         {
             if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
             {
-                popUpIndex++;
+                changeIndex();
             }
         }
-
         else if (popUpIndex == 1)
         {
-            if (Input.GetKeyDown(KeyCode.W))
+            if (Input.GetKeyDown(KeyCode.H))
             {
-                popUpIndex++;
+                changeIndex();
             }
         }
         else if (popUpIndex == 2)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                popUpIndex++;
+                changeIndex();
             }
-
         }
         else if (popUpIndex == 3)
         {
             if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
             {
-                popUpIndex++;
+                changeIndex();
             }
-
         }
-        else if (popUpIndex == 3)
+        else if (popUpIndex == 4)
         {
-            if (Input.GetKeyDown(KeyCode.UpArrow))
+            if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                popUpIndex++;
+                changeIndex();
             }
-
         }
+        else if (popUpIndex == 5)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                changeIndex();
+            }
+        }
+
 
     }
 }
