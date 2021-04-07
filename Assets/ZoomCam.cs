a75dd67bc0c130ded5d48f9c02b8bf7df0fbf9d4
kuguardt <kuguardt@gmail.com>
2021-04-07 13:17:08 +0700

    map2 จัดฉาก

--- Assets/ZoomCam.cs
+++ Assets/ZoomCam.cs
@@ -5,9 +5,6 @@ using Cinemachine;
 
 public class ZoomCam : MonoBehaviour
 {
-    public bool ZoomActive = false;
-    public float sizetozoom = 6.0f;
-    public float normalsize = 5.0f;
     CinemachineVirtualCamera virtualcam;
     void Start()
     {
